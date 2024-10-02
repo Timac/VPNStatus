87f7335056971e39da5fe70e72e74e364fc772f8
Alexandre Colucci <git-no-reply@colucci.fr>
2024-10-02 21:25:54 +0200

    Fix spacing

--- Common/ACNEServicesManager.m
+++ Common/ACNEServicesManager.m
@@ -21,40 +21,39 @@
 	{
 		sSharedNEServicesManager = [[ACNEServicesManager alloc] init];
 	}
-	
+
 	return sSharedNEServicesManager;
 }
 
 - (instancetype)init
 {
-    self = [super init];
-    if (self)
-    {
-    	// Create the dispatch queue
-        _neServiceQueue = dispatch_queue_create("Network Extension service Queue", NULL);
-		
-        // Allocate the NEServices array
-        _neServices = [[NSMutableArray alloc] init];
-    }
-    return self;
+	self = [super init];
+	if (self)
+	{
+		// Create the dispatch queue
+		_neServiceQueue = dispatch_queue_create("Network Extension service Queue", NULL);
+
+		// Allocate the NEServices array
+		_neServices = [[NSMutableArray alloc] init];
+	}
+	return self;
 }
 
 -(void) loadConfigurationsWithHandler:(void (^)(NSError * error))handler
 {
 	// Load the NEConfigurations
-    [[NEConfigurationManager sharedManager] loadConfigurationsWithCompletionQueue:[self neServiceQueue] handler:^(NSArray<NEConfiguration *> * neConfigurations, NSError * error)
-    {
+	[[NEConfigurationManager sharedManager] loadConfigurationsWithCompletionQueue:[self neServiceQueue] handler:^(NSArray<NEConfiguration *> * neConfigurations, NSError * error)
+	{
 		if(error != nil)
 		{
 			NSLog(@"ERROR loading configurations - %@", error);
 			return;
 		}
-		
-		dispatch_async(dispatch_get_main_queue(), ^
-		{
+
+		dispatch_async(dispatch_get_main_queue(), ^{
 			// Process the configurations
 			[self processConfigurations:neConfigurations];
-			
+
 			handler(error);
 		});
 	}];
@@ -66,7 +65,7 @@
 	[self.neServices removeAllObjects];
 
 	NSArray<NSString *>* ignoredVPNs = [[ACPreferences sharedPreferences] ignoredVPNs];
-	
+
 	for(NEConfiguration *neConfiguration in inConfigurations)
 	{
 		if([ignoredVPNs containsObject:neConfiguration.name])
@@ -74,21 +73,21 @@
 			// Don't show the VPNs that should be ignored
 			continue;
 		}
-		
+
 		ACNEService *service = [[ACNEService alloc] initWithConfiguration:neConfiguration];
 		[self.neServices addObject:service];
 	}
-	
+
 	// Sort by name
 	[self.neServices sortUsingComparator:^NSComparisonResult(ACNEService *obj1, ACNEService *obj2)
 	{
 		return [obj1.name compare:obj2.name];
 	}];
-	
+
 	// Refresh the states
 	for(ACNEService *neService in self.neServices)
 	{
-		[neService refreshSession]; 
+		[neService refreshSession];
 	}
 }
 
